fbc0aa52072cdc00f201a2c1ba4895a5696f2c62
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2016-06-07 22:42:43 +0800

    Add setup script

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+
+version = __import__('everywhere').version
+exclude_from_packages = []
+requires = []
+
+
+setup(
+    name='python-everywhere',
+    version=version,
+    url='https://YOUR_URL/',
+    author='Chiu-Hsiang Hsu',
+    author_email='wdv4758h@gmail.com',
+    description=('A template project for Python'),
+    license='BSD',
+    install_requires=requires,
+    packages=find_packages(exclude=exclude_from_packages),
+    include_package_data=True,
+    scripts=[],
+    extras_require={},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
