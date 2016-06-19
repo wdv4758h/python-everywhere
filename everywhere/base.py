74d1ea9c2d56790cff521ff073e0ac0419250a6a
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2016-06-19 21:01:12 +0800

    Remove redundant else

--- everywhere/base.py
+++ everywhere/base.py
@@ -13,8 +13,7 @@ def fib(number: int) -> int:
     """
     if number < 2:
         return number
-    else:
-        return fib(number-1) + fib(number-2)
+    return fib(number-1) + fib(number-2)
 
 
 def hello() -> str:
